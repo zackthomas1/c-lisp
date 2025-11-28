b1cd9e4d5008e4ca52815d4eacb6ce39c038e32b
zackthomas1 <zackthomas13@gmail.com>
2025-11-27 21:07:41 -0800

    Refactor input handling in main.c to improve readability and maintainability

--- main.c
+++ main.c
@@ -1,22 +1,44 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-// Declare a bufffer for useer input of size 2048
-static char input[2048];
+// Declare a bufffer for user input of size 2048
+static char buffer[2048];
+
+// If compiling on Windows include these libraries
+#ifdef _WIN32
+#include <string.h>
+
+char* readline(char* prompt){ 
+    fputs(prompt, stdout); 
+    fgets(buffer, 2048, stdin);
+    char* cpy = malloc(strlen(buffer)+1); 
+    strcpy(cpy, buffer);
+    cpy[strlen(cpy)-1] = '\0';
+    return cpy;
+}
+
+void add_history(char* unused) {}
+
+#else
+#include <editline/readline.h>
+#include <editline/history.h>
+#endif
 
 int main(int argc, char** argv){
     // Print version and exit information
     puts("Lispy Version 0.0.0.0.1");
     puts("Press Ctrl+c to exit\n"); 
 
+    // REPL (Read Eval Print Loop)
     while (1) {
         // output prompt
-        fputs("lispy> ", stdout); 
-
-        // read line of user input. Maximum is zee 2048
-        fgets(input, 2048, stdin);
+        char* input = readline("lispy> ");
+        add_history(input);
 
         // Echo input back to user
-        printf("No you're a %s", input);
+        printf("No you're a %s\n", input);
+
+        free(input);
     }
 
     return 0; 
